d3fccdfd60683a44e67b8c01ca8018bb1e843e9c
Erdi <kaygusuzerdi21@gmail.com>
2026-06-02 21:34:01 +0300

    add webview

---
+++ hopptr/src/app/index.tsx
@@ -0,0 +1,18 @@
+import { WebView } from 'react-native-webview';
+import { StyleSheet, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <WebView 
+        source={{ uri: 'https://hopp-sunucu-production.up.railway.app' }}
+        style={styles.webview}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  webview: { flex: 1 },
+});
