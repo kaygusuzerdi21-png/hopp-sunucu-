8a5e69e9f5c710ede9e0342b4709689ce4ac2e17
Erdi <kaygusuzerdi21@gmail.com>
2026-06-04 20:59:33 +0300

    netlify url update

--- hopptr/src/app/index.tsx
+++ hopptr/src/app/index.tsx
@@ -1,18 +1,12 @@
 import { WebView } from 'react-native-webview';
 import { StyleSheet, View } from 'react-native';
-import { useAssets } from 'expo-asset';
 
 export default function App() {
-  const [assets] = useAssets([require('../../assets/index.html')]);
-  
   return (
     <View style={styles.container}>
       <WebView 
-        source={assets ? { uri: assets[0].localUri! } : { html: '<h1>Yükleniyor...</h1>' }}
+        source={{ uri: 'https://hoppapk.netlify.app/' }}
         style={styles.webview}
-        allowFileAccess={true}
-        allowUniversalAccessFromFileURLs={true}
-        originWhitelist={['*']}
       />
     </View>
   );
